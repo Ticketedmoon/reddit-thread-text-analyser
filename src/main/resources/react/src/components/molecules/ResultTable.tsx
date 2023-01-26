8adfdf4e9f4f27b09e213330227d8d5ffb5977f6
Ticketedmoon <shane.creedon3@mail.dcu.ie>
2023-01-26 22:52:25 +0000

    main: Sorting + Pagination for ResultTable.tsx
    Signed-off-by: Ticketedmoon <shane.creedon3@mail.dcu.ie>

--- src/main/resources/react/src/components/molecules/ResultTable.tsx
+++ src/main/resources/react/src/components/molecules/ResultTable.tsx
@@ -7,7 +7,8 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import {RedditTextType} from "../types/RedditTextType";
-import {styled, tableCellClasses} from "@mui/material";
+import {Box, styled, tableCellClasses, TablePagination, TableSortLabel} from "@mui/material";
+import {visuallyHidden} from '@mui/utils';
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
     [`&.${tableCellClasses.head}`]: {
@@ -37,36 +38,208 @@ export interface ResultTableRow {
     count: number
 }
 
-export const ResultTable = (props: {rows: ResultTableRow[]}): JSX.Element => {
+interface HeadCell {
+    id: (keyof ResultTableRow);
+    numeric: boolean;
+    disablePadding: boolean;
+    label: string;
+}
+
+const headCells: readonly HeadCell[] = [
+    {
+        id: 'id',
+        numeric: false,
+        disablePadding: true,
+        label: 'ID',
+    },
+    {
+        id: 'type',
+        numeric: false,
+        disablePadding: false,
+        label: 'Type',
+    },
+    {
+        id: 'textItem',
+        numeric: false,
+        disablePadding: false,
+        label: 'Text',
+    },
+    {
+        id: 'count',
+        numeric: true,
+        disablePadding: false,
+        label: 'Count',
+    }
+];
+
+type Order = 'asc' | 'desc';
+
+interface EnhancedTableProps {
+    numSelected: number;
+    onRequestSort: (event: React.MouseEvent<unknown>, property: keyof ResultTableRow) => void;
+    onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    order: Order;
+    orderBy: string;
+    rowCount: number;
+}
+
+const EnhancedTableHead = (props: EnhancedTableProps) => {
+    const {order, orderBy, onRequestSort} = props;
+    const createSortHandler = (property: keyof ResultTableRow) => (event: React.MouseEvent<unknown>) => {
+        onRequestSort(event, property);
+    };
 
     return (
-        <TableContainer component={Paper}>
-            <Table sx={{ width: 0.5 }}
-                   size="small"
-                   aria-label="result table">
-                <TableHead>
-                    <TableRow>
-                        <StyledTableCell>ID</StyledTableCell>
-                        <StyledTableCell>Type</StyledTableCell>
-                        <StyledTableCell>Text</StyledTableCell>
-                        <StyledTableCell>Count</StyledTableCell>
-                    </TableRow>
-                </TableHead>
+        <TableHead>
+            <TableRow>
+                {headCells.map((headCell) => (
+                    <StyledTableCell key={headCell.id}
+                                     align={headCell.numeric ? 'right' : 'left'}
+                                     padding={headCell.disablePadding ? 'none' : 'normal'}
+                                     sortDirection={orderBy === headCell.id ? order : false}>
+                        <TableSortLabel active={orderBy === headCell.id}
+                                        direction={orderBy === headCell.id ? order : 'asc'}
+                                        onClick={createSortHandler(headCell.id)}>
+                            {headCell.label}
+                            {orderBy === headCell.id ? (
+                                <Box component="span" sx={visuallyHidden}>
+                                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                                </Box>
+                            ) : null}
+                        </TableSortLabel>
+                    </StyledTableCell>
+                ))}
+            </TableRow>
+        </TableHead>
+    );
+}
 
-                <TableBody>
-                    {props.rows.map((row: ResultTableRow) => (
-                        <StyledTableRow key={row.id}>
-                            <StyledTableCell component="th" scope="row">
-                                {row.id}
-                            </StyledTableCell>
+export const ResultTable = (props: {rows: ResultTableRow[]}): JSX.Element => {
 
-                            <StyledTableCell>{row.type}</StyledTableCell>
-                            <StyledTableCell>{row.textItem}</StyledTableCell>
-                            <StyledTableCell>{row.count}</StyledTableCell>
-                        </StyledTableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+    const [order, setOrder] = React.useState<Order>('asc');
+    const [orderBy, setOrderBy] = React.useState<keyof ResultTableRow>('count');
+    const [selected, setSelected] = React.useState<readonly string[]>([]);
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+    const handleRequestSort = (
+        event: React.MouseEvent<unknown>,
+        property: keyof ResultTableRow,
+    ) => {
+        const isAsc = orderBy === property && order === 'asc';
+        setOrder(isAsc ? 'desc' : 'asc');
+        setOrderBy(property);
+    };
+
+    const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.checked) {
+            const newSelected = props.rows.map((n) => n.id);
+            setSelected(newSelected);
+            return;
+        }
+        setSelected([]);
+    };
+
+    const handleClick = (event: React.MouseEvent<unknown>, name: string) => {
+        const selectedIndex = selected.indexOf(name);
+        let newSelected: readonly string[] = [];
+
+        if (selectedIndex === -1) {
+            newSelected = newSelected.concat(selected, name);
+        } else if (selectedIndex === 0) {
+            newSelected = newSelected.concat(selected.slice(1));
+        } else if (selectedIndex === selected.length - 1) {
+            newSelected = newSelected.concat(selected.slice(0, -1));
+        } else if (selectedIndex > 0) {
+            newSelected = newSelected.concat(
+                selected.slice(0, selectedIndex),
+                selected.slice(selectedIndex + 1),
+            );
+        }
+
+        setSelected(newSelected);
+    };
+
+    const handleChangePage = (event: unknown, newPage: number) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    const isSelected = (name: string) => selected.indexOf(name) !== -1;
+
+    function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+        if (b[orderBy] < a[orderBy]) {
+            return -1;
+        }
+        if (b[orderBy] > a[orderBy]) {
+            return 1;
+        }
+        return 0;
+    }
+
+    function getComparator<Key extends keyof any>(
+        order: Order,
+        orderBy: Key,
+    ): (
+        a: { [key in Key]: number | string },
+        b: { [key in Key]: number | string },
+    ) => number {
+        return order === 'desc'
+            ? (a, b) => descendingComparator(a, b, orderBy)
+            : (a, b) => -descendingComparator(a, b, orderBy);
+    }
+
+    return (
+        <Box sx={{width: 0.5}}>
+            <Paper sx={{width: 1, mb: 2}}>
+                <TableContainer>
+                    <Table sx={{width: 1}}
+                           size="small"
+                           aria-label="result table">
+                        <EnhancedTableHead numSelected={selected.length}
+                                           order={order}
+                                           orderBy={orderBy}
+                                           onSelectAllClick={handleSelectAllClick}
+                                           onRequestSort={handleRequestSort}
+                                           rowCount={props.rows.length}/>
+
+                        <TableBody>
+                            {props.rows.slice().sort(getComparator(order, orderBy))
+                                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                .map((row) => {
+                                    const isItemSelected = isSelected(row.id);
+                                    return (
+                                        <StyledTableRow key={row.id}
+                                                        onClick={(event) => handleClick(event, row.id)}
+                                                        aria-checked={isItemSelected}
+                                                        tabIndex={-1}
+                                                        selected={isItemSelected}>
+                                            <StyledTableCell component="th" scope="row">
+                                                {row.id}
+                                            </StyledTableCell>
+                                            <StyledTableCell>{row.type}</StyledTableCell>
+                                            <StyledTableCell>{row.textItem}</StyledTableCell>
+                                            <StyledTableCell align="right">{row.count}</StyledTableCell>
+                                        </StyledTableRow>
+                                    )
+                                })}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <TablePagination
+                    rowsPerPageOptions={[5, 10, 25]}
+                    component="div"
+                    count={props.rows.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onPageChange={handleChangePage}
+                    onRowsPerPageChange={handleChangeRowsPerPage}
+                />
+            </Paper>
+        </Box>
     );
 }
