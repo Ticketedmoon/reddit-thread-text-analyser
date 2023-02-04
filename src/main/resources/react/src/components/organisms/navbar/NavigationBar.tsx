3dc9fc0bff313bf4f7516eefa08d78ddb4c3afa0
Ticketedmoon <shane.creedon3@mail.dcu.ie>
2023-02-04 17:26:57 +0000

    main: Remove PageState from NavigationBar.tsx
    Signed-off-by: Ticketedmoon <shane.creedon3@mail.dcu.ie>

--- src/main/resources/react/src/components/organisms/navbar/NavigationBar.tsx
+++ src/main/resources/react/src/components/organisms/navbar/NavigationBar.tsx
@@ -1,31 +1,13 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 
 import {Box, Typography} from "@mui/material";
-import {Outlet, useLocation, useNavigate} from "react-router-dom";
-
-enum PageState {
-    JOB_CREATION_PAGE,
-    COMPLETED_JOBS_PAGE
-}
+import {Outlet, useNavigate} from "react-router-dom";
 
 export const NavigationBar = (): JSX.Element => {
 
     const navigate = useNavigate();
-    const location = useLocation();
 
-    const [page, setPage] = useState<PageState>(PageState.JOB_CREATION_PAGE);
-
-    useEffect(() => {
-        const path = location.pathname;
-        if (path === "/") {
-            setPage(PageState.JOB_CREATION_PAGE);
-        } else if (path == "/results") {
-            setPage(PageState.COMPLETED_JOBS_PAGE);
-        }
-    }, []);
-
-    const handleTabChange = (pageState: PageState, routeForDirect: string) => {
-        setPage(pageState);
+    const handleTabChange = (routeForDirect: string) => {
         navigate(routeForDirect);
     }
 
@@ -44,7 +26,7 @@ export const NavigationBar = (): JSX.Element => {
                              cursor: 'pointer'
                          }
                      }}
-                     onClick={() => handleTabChange(PageState.JOB_CREATION_PAGE, "/")}>
+                     onClick={() => handleTabChange("/")}>
                     <Typography variant="subtitle1" sx={{
                         fontFamily: "MyItimFont, arial, sans-serif",
                         fontSize: "1.2em",
@@ -64,7 +46,7 @@ export const NavigationBar = (): JSX.Element => {
                              cursor: 'pointer'
                          }
                      }}
-                    onClick={() => handleTabChange(PageState.COMPLETED_JOBS_PAGE, "/results")}>
+                    onClick={() => handleTabChange("/results")}>
                     <Typography variant="subtitle1" sx={{
                         color: "whitesmoke",
                         fontSize: "1.2em",
